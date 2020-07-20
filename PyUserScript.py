0b5c0331cc839f1042350cc34da7ef055e79fbb1
ahmetlii <63475369+ahmetlii@users.noreply.github.com>
2020-07-20 17:17:55 +0300

    Add files via upload

--- PyUserScript.py
+++ PyUserScript.py
@@ -22,12 +22,11 @@ for template in wikicode.filter_templates():
           lister=[user+users]
           listread=wp.page(lister)
           contents=listread.read()
-     break
-print ("Are you sure? It will send the invitations in next step. Please check limitations about invitations, this tool has not liability and guarantee on edits.(y for if you are sure)")
-keyboard.wait("y")
-print("Write the page exactly that you want to invite")
-input(link)      
-contents += "==Discussion Invitation== \n {{Discussion Invitation|"+link+"}} \n If you don't want to take invitations automatically, please contact with the [[User talk:"+username+"|"username"]] for invitations or the [[User:Ahmetlii|programmer]] for errors."
-summary = "Sent an invitation with [[User:Ahmetlii/PyUserScript|PyUserScript]]"
-print("Invitations sent by autoscript. Please exit from the terminal.")
-time.sleep(3)
+          print ("Are you sure? It will send the invitations in next step. Please check limitations about invitations, this tool has not liability and guarantee on edits.(y for if you are sure)")
+          keyboard.wait("y")
+          print("Write the page exactly that you want to invite")
+          input(link)      
+          contents += "==Discussion Invitation== \n {{Discussion Invitation|"+link+"}} \n If you don't want to take invitations automatically, please contact with the [[User talk:"+username+"|"+username+"]] for invitations or the [[User:Ahmetlii|programmer]] for errors."
+          summary = "Sent an invitation with [[User:Ahmetlii/PyUserScript|PyUserScript]]"
+          print("Invitations sent by autoscript. Please exit from the terminal.")
+          time.sleep(3)
