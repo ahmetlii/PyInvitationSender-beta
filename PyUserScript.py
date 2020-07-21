da62f244be780b1910a8ffc6b54302f0cd40c5be
ahmetlii <63475369+ahmetlii@users.noreply.github.com>
2020-07-21 22:37:19 +0300

    Add files via upload

--- PyUserScript.py
+++ PyUserScript.py
@@ -21,8 +21,9 @@ for template in wikicode.filter_templates():
           for x in lister:
                listread = wp.page(x)
                contents = listread.read()
-               contents += '\n==Discussion Invitation==\n{{Discussion Invitation|'+link+'}}\nThis invitation sent by [[User:Ahmetlii/PyUserScript|PyUserScript]]. If you do not want to take invitations automatically, please contact with the [[User talk:'+username+'|'+username+']] for invitations or the [[User talk:Ahmetlii|programmer]] for errors.'
+               contents += '\n==Discussion Invitation==\n{{Discussion Invitation|'+link+'}}\nThis invitation sent by [[User:Ahmetlii/PyUserScript|PyUserScript]]. If you do not want to take invitations automatically, please contact with the [[User talk:'+username+'|'+username+']] for invitations or the [[User talk:Ahmetlii|programmer]] for errors.~~~~'
                summary = "Sent an invitation with [[User:Ahmetlii/PyUserScript|PyUserScript]]"
                listread.edit(contents, summary)
                repeat=repeat+1
                print('An invitation sent by autoscript. Close the terminal when all of them are sent. It will stop when all of them are sent.')
+
