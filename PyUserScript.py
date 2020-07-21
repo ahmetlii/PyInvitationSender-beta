0cc42e34ba38d51188e46acf4ecafc8b9094d85b
ahmetlii <63475369+ahmetlii@users.noreply.github.com>
2020-07-21 21:45:10 +0300

    Add files via upload

--- PyUserScript.py
+++ PyUserScript.py
@@ -3,33 +3,23 @@ import keyboard  # using module keyboard
 import re
 import mwparserfromhell
 import time
-username = "\n"
-password = "\n"
-link = "\n"
 wp = mw.Wiki("https://en.scratch-wiki.info/w/api.php", "PyUserScript version 0.4.1")
-print ("Please write your username, then press enter")
-input (username)
-print ("Password?")
-input (password)
+username = input('Please write your username, then press enter\n')
+password = input('Password?\n')
 wp.clientlogin(username, password)
 invitesender = wp.page("Scratch_Wiki:Discussion_Invitation/List")
-user='User talk:'
+user = 'User talk:'
 wikicode=mwparserfromhell.parse(invitesender)
 item=wikicode.filter_templates(recursive=False)[0]
 for template in wikicode.filter_templates():
      if template.name.matches("/item"):
-          users=str(template.get(1).value)     
-          lister=[user+users]
-          listread=wp.page(lister)
-          contents=listread.read()
-          print ("Are you sure? It will send the invitations in next step. Please check limitations about invitations, this tool has not liability and guarantee on edits.(y for if you are sure)")
-          keyboard.wait("y")
-          print ("You agreed.")
-          time.sleep(3)
-          print("Write the page exactly that you want to invite")
-          input(link)      
-          contents += "==Discussion Invitation== \n {{Discussion Invitation|"+link+"}} \n If you don't want to take invitations automatically, please contact with the [[User talk:"+username+"|"+username+"]] for invitations or the [[User talk:Ahmetlii|programmer]] for errors."
+          users = str(template.get(1).value)     
+          lister = [user+users]
+          listread = wp.page(lister)
+          contents = listread.read()
+          link = input('Write the page exactly that you want to invite\n')
+          repeater = len(lister)
+          contents += '\n==Discussion Invitation==\n{{Discussion Invitation|'+link+'}}\nThis invitation sent by [[User:Ahmetlii/PyUserScript|PyUserScript]].If you do not want to take invitations automatically, please contact with the [[User talk:'+username+'|'+username+']] for invitations or the [[User talk:Ahmetlii|programmer]] for errors.'
           summary = "Sent an invitation with [[User:Ahmetlii/PyUserScript|PyUserScript]]"
           listread.edit(contents, summary)
           print("Invitations sent by autoscript. Please exit from the terminal.")
-          time.sleep(3000)
