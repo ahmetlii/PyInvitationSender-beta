3ac93b6ecfef1b8cc498759d65040b71ba9a41b9
ahmetlii <63475369+ahmetlii@users.noreply.github.com>
2020-07-21 22:45:31 +0300

    done?

--- PyUserScript.py
+++ PyUserScript.py
@@ -7,6 +7,7 @@ wp = mw.Wiki("https://en.scratch-wiki.info/w/api.php", "PyUserScript version 0.4
 username = input('Please write your username, then press enter\n')
 password = input('Password?\n')
 wp.clientlogin(username, password)
+link = input('Write the page exactly that you want to invite\n')
 invitesender = wp.page("Scratch_Wiki:Discussion_Invitation/List")
 user = 'User talk:'
 wikicode=mwparserfromhell.parse(invitesender)
@@ -16,14 +17,10 @@ for template in wikicode.filter_templates():
      if template.name.matches("/item"):
           users = str(template.get(1).value)     
           lister = [user+users]
-          link = input('Write the page exactly that you want to invite\n')
-          allpos = len(lister)
           for x in lister:
                listread = wp.page(x)
                contents = listread.read()
                contents += '\n==Discussion Invitation==\n{{Discussion Invitation|'+link+'}}\nThis invitation sent by [[User:Ahmetlii/PyUserScript|PyUserScript]]. If you do not want to take invitations automatically, please contact with the [[User talk:'+username+'|'+username+']] for invitations or the [[User talk:Ahmetlii|programmer]] for errors.~~~~'
                summary = "Sent an invitation with [[User:Ahmetlii/PyUserScript|PyUserScript]]"
                listread.edit(contents, summary)
-               repeat=repeat+1
                print('An invitation sent by autoscript. Close the terminal when all of them are sent. It will stop when all of them are sent.')
-
