246887a90f3757a17adad497ec09e39187c3ef78
ahmetlii <63475369+ahmetlii@users.noreply.github.com>
2020-07-21 23:38:57 +0300

    Update PyUserScript.py

--- PyUserScript.py
+++ PyUserScript.py
@@ -22,4 +22,4 @@ for template in wikicode.filter_templates():
                contents += '\n==Discussion Invitation==\n{{Discussion Invitation|'+link+'}}\nThis invitation sent by [[User:Ahmetlii/PyUserScript|PyUserScript]]. If you do not want to take invitations automatically, please contact with the [[User talk:'+username+'|'+username+']] for invitations or the [[User talk:Ahmetlii|programmer]] for errors.~~~~'
                summary = "Sent an invitation with [[User:Ahmetlii/PyUserScript|PyUserScript]]"
                listread.edit(contents, summary)
-               print('An invitation sent by autoscript. Close the terminal when all of them are sent. It will stop when all of them are sent.')
+               print('An invitation sent by autoscript. It will close when all of them are sent.')
